826b27a8c6f2213db2d12323f54e4b26d03357cc
AK3103 <155659249+AK3103@users.noreply.github.com>
2024-01-05 19:18:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,13 @@ BOT_TOKEN =""
 Animelist = Client(
     name="Aniume-bot",
     api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+  )  
+
+
+  print("Bot has Started")
+
+  Animelist.run() 
+ 
     
