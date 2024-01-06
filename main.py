027f8645e7edc92b914572ee3282687ff4881880
AK3103 <155659249+AK3103@users.noreply.github.com>
2024-01-06 19:36:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from pyrogram import Client, filters
 
 
-API_ID = ""
-API_HASH = ""
-BOT_TOKEN =""
+API_ID = "23472307"
+API_HASH = "15311dad78288ffec031dc8b8b717c21"
+BOT_TOKEN ="6946934155:AAFx4VojSkQyTf6sx8lvhbsC8U-2Wb_yEok"
 
 Animelist = Client(
     name="Aniume-bot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
-  )  
+  )
+
+
+  @Aniumelist.on_message(filters.command("
+
+
+   
 
 
   print("Bot has Started")
